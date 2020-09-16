be5abd8dd5e785710086134aa32d4d4b002f79aa
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-16 22:18:26 +0000

    02-Templates Inheritance and CSS wired

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Niall Ferguson',
+        'title': 'The Square and The Tower',
+        'content': 'History of The Social Networks',
+        'date_posted': 'March 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Second Blog II',
+        'content': 'Posted Content',
+        'date_posted': 'August 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>New Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
