63d62def3ea0ce2e22f9152f143b4acea07cdfdf
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-16 23:11:30 +0000

    04-Datebase added. Dummy data dictionary removed

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Niall Ferguson',
-        'title': 'The Square and The Tower',
-        'content': 'History of The Social Networks',
-        'date_posted': 'March 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Second Blog II',
-        'content': 'Posted Content',
-        'date_posted': 'August 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
