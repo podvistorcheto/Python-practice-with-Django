69ff7496cd145b30e4f3b844f3aa9ee728285966
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-17 13:10:42 +0000

    10.02 CRUD part 2 to create/read a post completed

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -21,7 +26,7 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
