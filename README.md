cb7acf2c01b4ebc1f54616cfb5c3a242e1df121d
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-18 12:02:07 +0000

    11 - Pagination and blogs per each profile

--- README.md
+++ README.md
@@ -5,3 +5,53 @@ This is full-feature app for my history blog.
 It is programmed as a self-study tutorial for Python3 with Django 3.1.1.
 
 Enjoy exploring the code!
+
+Python Django Tutorial: Full-Featured Web App Part 11 - Pagination
+
+To code in the shell part type django shell if you use django-shortcuts or regular command python manage.py shell. The type:
+
+In [1]: from django.core.paginator import Paginator
+
+In [2]: posts = ['1','2','3','4','5']
+
+In [3]: p = Paginator(posts,2)
+
+In [4]: p.num_pages
+Out[4]: 3
+
+In [5]: for page in p.page_range:
+   ...:     print(page)
+   ...: 
+1
+2
+3
+
+In [6]: p1 = p.page(1)
+
+In [7]: p1
+Out[7]: <Page 1 of 3>
+
+In [8]: p1.number
+Out[8]: 1
+
+In [9]: p1.object_list
+Out[9]: ['1', '2']
+
+In [10]: p1.has_previous()
+Out[10]: False
+
+In [11]: p1.has_next()
+Out[11]: True
+
+In [12]: p1.has.next_page_number()
+---------------------------------------------------------------------------
+AttributeError                            Traceback (most recent call last)
+<ipython-input-12-2041b3b011bd> in <module>
+----> 1 p1.has.next_page_number()
+
+AttributeError: 'Page' object has no attribute 'has'
+
+In [13]: p1.next_page_number()
+Out[13]: 2
+
+In [14]: exit()
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,6 +8,7 @@ from django.views.generic import (
 )
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -19,9 +20,22 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'   # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'   # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -50,6 +64,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
